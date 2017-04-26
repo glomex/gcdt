152a61189ecf9b62cf156c3a9f99b4596125d0b9
mark <mark@mark-fink.de>
2017-04-26 18:14:53 +0200

    #250 credentials binding

--- infrastructure/jenkins/jobs/gcdt_develop.groovy
+++ infrastructure/jenkins/jobs/gcdt_develop.groovy
@@ -8,7 +8,6 @@ def branchToCheckout = InfraUtilities.getBranch()
 
 out.println(branchToCheckout)
 
-//def credentialsToCheckout = "glomex-sre-deploy"
 def credentialsToCheckout = "github-jenkins_username-password"
 def baseFolder = "infrastructure/jenkins"
 def buildScript = baseFolder + "/scripts/build_develop.sh"
