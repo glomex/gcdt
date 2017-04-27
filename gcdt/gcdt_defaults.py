4245f02de92f7025a0651607b87bbfa7e285b89d
mark <mark@mark-fink.de>
2017-04-27 10:24:59 +0200

    #254 guard / prepare for multiple runtimes

--- gcdt/gcdt_defaults.py
+++ gcdt/gcdt_defaults.py
@@ -21,7 +21,8 @@ hardcoded values.
 
 DEFAULT_CONFIG = {
     'ramuda': {
-        'settings_file': 'settings.json'
+        'settings_file': 'settings.json',
+        'runtime': ['python2.7', 'nodejs4.3']
     }
 }
 
